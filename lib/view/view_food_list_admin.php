6dbf60d74c7242f94911b5ae5dd2888aaf1a703a
book-wing <50644809+book-wing@users.noreply.github.com>
2019-05-22 16:55:56 +0900

    生鮮食品一覧画面修正

--- lib/view/view_food_list_admin.php
+++ lib/view/view_food_list_admin.php
@@ -1,6 +1,6 @@
 <!-- /**
  * このファイルの概要説明
-分類一覧画面（IT担当社員）
+ 生鮮食品一覧画面
  *　データベース作成フォーマット
  * このファイルの詳細説明
  *
@@ -16,11 +16,11 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>分類一覧画面(IT担当者)</title>
+<title>生鮮食品一覧画面(IT担当者)</title>
 </head>
 <body>
 <input type="submit" value="ログアウト">
-分類一覧画面
+生鮮食品一覧画面
 <!-- 画面上部タブ -->
 <a href="">特価商品</a>
 <a href="">生鮮食品</a>
@@ -29,6 +29,7 @@
 <a href="">店舗</a>
 
   <!-- 検索窓作成 -->
+
     <form action="view_food_list_admin.php" method="get">
       検索：<input type="text" name="search">
             <input type="submit" value="検索">
@@ -39,11 +40,19 @@
     </form>
     <hr>
     <table border="1">
+        <tr>
+            <th>写真</th>
+            <th>食品名</th>
+            <th>価格</th>
+            <th>説明文書</th>
+            <th>栄養素</th>
+        </tr>
       <tr>
-        <th>分類</th>
-      </tr>
-      <tr>
-        <td>分類</td>
+        <td>写真</td>
+        <td>食品名</td>
+        <td>価格</td>
+        <td>説明文書</td>
+        <td>栄養素</td>
       </tr>
     <form action = "food_update.php" method="post">
       <input type="submit" value="編集">
@@ -51,8 +60,9 @@
     <form action = "food_delete.php" method="post">
       <input type="submit" value="削除">
     </form>
+    表示<input type="radio" name="show" value="表示">
+    非表示<input type="radio" name="show" value="非表示">
   <a href="">全て表示</a>
-
 </table>
 </body>
 </html>
