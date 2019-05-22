e1c23374932c85144e908e5e9229f69301b82d62
Ryoitym <44043239+Ryoitym@users.noreply.github.com>
2019-05-22 14:47:01 +0900

    データベース作成完了
    茄子データ追加

--- ffs_db.sql
+++ ffs_db.sql
@@ -14,7 +14,7 @@
 
 --　データベース作成
 DROP DATABASE IF EXISTS ffs_db;
-CREATE DATABASE ffs_db DEFAULT CHARACTER SET UTF-8;
+CREATE DATABASE ffs_db DEFAULT CHARACTER SET UTF8;
 
 --　ユーザ作成
 GRANT ALL PRIVILEGES ON ffs_db.* TO 'root'@'%' IDENTIFIED BY 'ffs';
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS food (
   `food_name` VARCHAR(100) NOT NULL,
   `food_price` INT NOT NULL,
   `picture` VARCHAR(300) NOT NULL,
-  `text` VARCHAR NOT NULL,
+  `txt` TEXT NOT NULL,
   `calorie` FLOAT NOT NULL,
   `protein` FLOAT,
   `lipid` FLOAT,
@@ -43,26 +43,104 @@ CREATE TABLE IF NOT EXISTS food (
   `show_flag` INT NOT NULL,
   `genre_id` INT NOT NULL,
   PRIMARY KEY (`food_id`)
-) ENGINE=InnoDB;
+);
 
--- mountain�e�[�u���f�[�^�C���T�[�g
-INSERT INTO mountain (name, kana, height, location) VALUES ('�ԃm�x',   '�����̂���',       3190, '��A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('�ԐΊx',   '������������',     3121, '��A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('���x',   '�����΂݂���',     3101, '�k�A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('���䍂�x', '�����ق�������',   3190, '�k�A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('��ԎR',   '���񂽂�����',     3067, '���̑�'    );
-INSERT INTO mountain (name, kana, height, location) VALUES ('����x',   '���炳�킾��',     3110, '�k�A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('�k�x',     '��������',         3193, '��A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('�k�䍂�x', '�����ق�������',   3106, '�k�A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('�����x',   '�����݂���',       3047, '��A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('��䃖�x', '���񂶂傤������', 3033, '��A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('���R',     '���Ă��',         3015, '�k�A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('���x',     '�Ȃ�����',         3084, '��A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('���x',     '�Ȃ�����',         3084, '�k�A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('�_���x',   '�̂��Ƃ肾��',     3051, '��A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('��Ɗx',   '�̂肭�炾��',     3026, '�k�A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('���x',     '�Ђ��肾��',       3013, '��A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('�O�䍂�x', '�܂��ق�������',   3090, '�k�A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('��x',     '�݂Ȃ݂���',       3033, '�k�A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('�����x',   '��肪����',       3180, '�k�A���v�X');
-INSERT INTO mountain (name, kana, height, location) VALUES ('����x',   '��邳�킾��',     3141, '��A���v�X');
+-- 店舗テーブル作成 shop
+DROP TABLE IF EXISTS shop;
+CREATE TABLE IF NOT EXISTS shop (
+  `shop_id` INT AUTO_INCREMENT NOT NULL,
+  `shop_name` VARCHAR(100) NOT NULL,
+  `address` VARCHAR(100) NOT NULL,
+  `tel` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`shop_id`)
+);
+
+-- ユーザテーブル作成 user
+DROP TABLE IF EXISTS user;
+CREATE TABLE IF NOT EXISTS user (
+  `user_id` INT AUTO_INCREMENT NOT NULL,
+  `password` VARCHAR(20) NOT NULL,
+  `name_family` VARCHAR(50) NOT NULL,
+  `name_last` VARCHAR(50) NOT NULL,
+  `mail` VARCHAR(300) NOT NULL,
+  `acess_lv` INT NOT NULL,
+  `shop_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`)
+);
+
+-- 特価品テーブル作成 sale
+DROP TABLE IF EXISTS sale;
+CREATE TABLE IF NOT EXISTS sale (
+  `sale_id` INT AUTO_INCREMENT NOT NULL,
+  `sale_price` INT NOT NULL,
+  `date` DATE NOT NULL,
+  `shop_id` INT NOT NULL,
+  `food_id` INT NOT NULL,
+  PRIMARY KEY (`sale_id`)
+);
+
+-- 分類テーブル作成 genre
+DROP TABLE IF EXISTS genre;
+CREATE TABLE IF NOT EXISTS genre (
+  `genre_id` INT AUTO_INCREMENT NOT NULL,
+  `genre_name` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`genre_id`)
+);
+--野菜
+INSERT INTO genre(
+  genre_id,
+  genre_name
+) VALUES (
+  1,
+  '野菜'
+);
+
+--肉
+INSERT INTO genre(
+  genre_id,
+  genre_name
+) VALUES (
+  2,
+  '肉'
+);
+
+--魚
+INSERT INTO genre(
+  genre_id,
+  genre_name
+) VALUES (
+  3,
+  '魚'
+);
+
+INSERT INTO food (
+  food_id, 
+  food_name,
+  food_price,
+  picture,
+  txt,
+  calorie,
+  protein,
+  lipid,
+  carb,
+  natrium,
+  kalium,
+  show_flag,
+  genre_id
+  ) VALUES (
+    1, 
+    '茄子',
+    60,
+    'http://placehold.jp/150x150.png',
+    '高知県産　みずみずしくて今が旬！',
+    22,
+    1.1,
+    0.1,
+    5.1,
+    0,
+    220,
+    1,
+    1
+    );
+
+
