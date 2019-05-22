7dc86939e690bba1833e522ded6a884d356e4f5f
book-wing <50644809+book-wing@users.noreply.github.com>
2019-05-22 17:32:45 +0900

    分類登録画面

---
+++ lib/view/view_category_insert.php
@@ -0,0 +1,33 @@
+<!-- /**
+ * このファイルの概要説明
+分類登録画面
+ *　データベース作成フォーマット
+ * このファイルの詳細説明
+ *
+ * システム名： FFS
+ * 作成者：　orange juice
+ * 作成日：　2019/05/22
+ * 最終更新日：　2019/05/22
+ * レビュー担当者：
+ * レビュー日：
+ * バージョン： 1.1
+ */ -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>分類登録画面</title>
+</head>
+<body>
+<input type="submit" value="ログアウト">
+分類登録画面
+
+分類名 <form action = "view_category_insert.php" method="post">
+        <input type="text" name="分類名">
+      </form>
+      <input type="submit" value="登録">
+      <input type="reset" value="クリア">
+  <a href="">全て表示</a>
+</table>
+</body>
+</html>
