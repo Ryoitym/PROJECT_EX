dc62d9dc773c3613da674c1e61098863a3c7dd04
owlViking <50614905+owlViking@users.noreply.github.com>
2019-05-22 15:41:55 +0900

    ログイン画面　HTML作成

---
+++ lib/view/view_login.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * このファイルの概要説明
+ *　ログイン画面作成フォーマット
+ * このファイルの詳細説明
+ *
+ * システム名： FFS
+ * 作成者：　sugerSong
+ * 作成日：　2019/05/22
+ * 最終更新日：　2019/05/22
+ * レビュー担当者：
+ * レビュー日：
+ * バージョン： 1.1
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ログイン画面</title>
+</head>
+<body>
+    <h1>ログイン画面</h1>
+    <form action="">
+        メールアドレス：<input type="text" value = ""><br>
+        パスワード：<input type="text" value = ""><br>
+        <input type="submit" value = "送信">
+    </form>
+</body>
+</html>
