23a07e28a75f895ffc0b3e9d14d2ea30fd2ee466
nosu10101 <50618690+nosu10101@users.noreply.github.com>
2019-05-22 17:02:58 +0900

    登録画面をHTML部分のみ作成 削除画面と変更画面のファイルを追加

---
+++ lib/view/view_special_price_food_insert.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>特価商品登録画面</title>
+    </head>
+    
+<body>
+<input type="submit" value="ログアウト"> <br>
+    <form action="view_special_price_food_insert.php" method="post">
+    店舗名: 
+    <select name="food_name">
+            <option>
+            てｓｔ
+            </option>
+            <option>
+            てｓｔ
+            </option>
+    </select>   
+    <br>
+    日付 <input type="text" name="search_name">
+    <br>
+    生鮮食品名: 
+    <select name="food_name">
+            <option>
+            てｓｔ
+            </option>
+            <option>
+            てｓｔ
+            </option>
+    </select>
+    <br>
+    価格 <input type="text" name="search_name">
+    
+    <br>
+    <input type="submit" value="登録">
+    <input type="submit" value="クリア">
+    </form>
+</body>
+</html>
