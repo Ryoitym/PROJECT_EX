5d978d0c402507bb6635eaf4c9e478e95d4d891f
owlViking <50614905+owlViking@users.noreply.github.com>
2019-05-23 09:53:38 +0900

    loginControlの大枠作成

---
+++ lib/login.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * このファイルの概要説明
+ *　login画面control作成フォーマット
+ * このファイルの詳細説明
+ *
+ * システム名： FFS
+ * 作成者：　sugerSong
+ * 作成日：　2019/05/23
+ * 最終更新日：　2019/05/23
+ * レビュー担当者：
+ * レビュー日：
+ * バージョン： 1.1
+ */
+
+
+//共通関数読み込み
+require_once("init.php");
+
+if(empty($_POST)){
+    require_once("view\view_login.php");
+} else{
+    if($_POST["mail"] == true){
+
+    }else{
+
+    }
+}
