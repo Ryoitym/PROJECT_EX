bc7de740ae6f13e8d30248e16490dfb4eab966fa
book-wing <50644809+book-wing@users.noreply.github.com>
2019-05-23 15:44:35 +0900

    分類削除確認画面

---
+++ category_delete.php
@@ -0,0 +1,48 @@
+<!--
+ * 分類削除確認画面
+ *
+ * システム名： FFS
+ * 作成者：　orange juice
+ * 作成日：　2019/05/23
+ * 最終更新日：　2019/05/23
+ * レビュー担当者：
+ * レビュー日：
+ * バージョン： 1.0
+ */
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>分類削除確認画面のコントローラ</title>
+</head>
+<body>
+<script>
+//ポップアップ
+var del = window.confirm("本当に削除しますか？");
+if(del){
+<?php
+    require_once("lib/function.php");
+    $dbh = connectDb();
+
+    try {
+        $sql = "DELETE FROM genre ";
+        $sql .= "WHERE genre_id=:genre_id";
+        $sth = $dbh->prepare($sql);
+
+        $sth->bindValue(":user_id", $_GET["genre_id"]);
+        $sth->execute();
+
+    } catch (PDOException $e) {
+        exit("SQL発行エラー：{$e->getMessage()}");
+    }
+?>
+}else{
+    window.location.replace('category_list_admin.php');
+}
+</script>
+<p>削除しました。</p>
+<p><a href="category_list_admin.php">分類一覧画面に戻る</a></p>
+</body>
+</html>
