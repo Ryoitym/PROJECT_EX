f64c315d3f05e4f748e9a03c1382afb596575510
nosu10101 <50618690+nosu10101@users.noreply.github.com>
2019-05-23 13:04:48 +0900

    特価商品一覧画面(店長)の作成----特価商品一覧画面(IT担当者)の修正

--- lib/view/view_special_price_food_list.php
+++ lib/view/view_special_price_food_list.php
@@ -1,12 +1,12 @@
 <!-- /**
  * このファイルの概要説明
- *　特価商品一覧画面
+ *　特価商品一覧画面(店長)
  * このファイルの詳細説明
  *
  * システム名： FFS
  * 作成者：　nosu10101
- * 作成日：　2019/05/22
- * 最終更新日：　2019/05/22
+ * 作成日：　2019/05/23
+ * 最終更新日：　2019/05/23
  * レビュー担当者：
  * レビュー日：
  * バージョン： 1.1
@@ -21,7 +21,6 @@
 <input type="submit" value="ログアウト">
 <a href="">特価商品</a>
 <a href="">生鮮食品</a>
-<a href="">分類</a>
 <a href="">ユーザ</a>
 <a href="">店舗</a>
 
@@ -29,6 +28,7 @@
 検索: <input type="text" name="search_name">
 <input type="submit" value="検索">
 </form>
+<input type="submit" value="新規登録">
 
 <!-- borderは後で消す -->
 <table border="1">
@@ -43,8 +43,8 @@
         <td>test</td>
         <td>test</td>
         <td>test</td>
-        <td></td>
-        <td></td>
+        <td><input type="submit" value="編集"></td>
+        <td><input type="submit" value="削除"></td>
     </tr>
 </table>
 
