dbb8dbbd00b0cbb4c043563eff4c5292f9bdd44f
book-wing <50644809+book-wing@users.noreply.github.com>
2019-05-23 11:55:32 +0900

    分類登録画面のコントローラー

---
+++ lib/model/category_insert.php
@@ -0,0 +1,57 @@
+<!-- /**
+ * このファイルの概要説明
+分類登録画面
+ *　データベース作成フォーマット
+ * このファイルの詳細説明
+ *
+ * システム名： FFS
+ * 作成者：　orange juice
+ * 作成日：　2019/05/23
+ * 最終更新日：　2019/05/23
+ * レビュー担当者：
+ * レビュー日：
+ * バージョン： 1.1
+ */ -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>分類登録画面</title>
+</head>
+<body>
+  <!-- 一画面遷移 -->
+  <?php
+      require_once("function.php");
+      //入力画面表示
+      if(empty($_POST)){
+        $dbh = connectDb();
+        require_once("view_category_insert.php");
+      }else{
+        //入力チェック
+        if(empty($_POST["genre_name"])){
+        // 入力NG
+            $dbh = connectDb();
+            require_once("view_category_insert.php");
+           ph("入力不十分です");
+        }else{
+          //入力チェックOK 分類を追加する
+          $dbh = connectDb();
+          try {
+              // プレースホルダ付きSQLを構築
+              $sql = "INSERT INTO genre (ganre_name) ";
+              $sql .= "VALUES (:genre_name)";
+              $sth = $dbh->prepare($sql); // SQLを準備
+
+              // プレースホルダに値をバインド
+              $sth->bindValue(":genre_name",$_POST["genre_name"]);
+
+              // SQLを発行
+              $sth->execute();
+          } catch (PDOException $e) {
+              exit("SQL発行エラー：{$e->getMessage()}");
+          }?>
+          <p><a href="view_category_list_admin.php">分類一覧画面に戻る</a></p>
+      <?php   }
+    } ?>
+  </body>
+</html>
