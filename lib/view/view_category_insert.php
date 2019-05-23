8c5da6841fb18a82e6c542d8febd9206078c9e77
book-wing <50644809+book-wing@users.noreply.github.com>
2019-05-23 11:55:04 +0900

    分類登録画面修正
    name属性修正

--- lib/view/view_category_insert.php
+++ lib/view/view_category_insert.php
@@ -23,10 +23,10 @@
 <h1>分類登録画面</h1>
 
 分類名 <form action = "view_category_insert.php" method="post">
-        <input type="text" name="分類名">
+        <input type="text" name="genre_name">
+        <input type="submit" value="登録">
+        <input type="reset" value="クリア">
       </form>
-      <input type="submit" value="登録">
-      <input type="reset" value="クリア">
 </table>
 </body>
 </html>
