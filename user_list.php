3a1a237e11b14afc73f9962ad4bc9bc833ad5437
amaru82 <kimberley1993@hotmail.co.jp>
2019-05-24 17:30:12 +0900

    修正

--- user_list.php
+++ user_list.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * このファイルの概要説明
  *　ログアウト画面作成フォーマット
@@ -14,38 +13,34 @@
  * バージョン： 1.1
  */
 
- require_once("lib/function.php");
- session_start();
- if($_SESSION["acess_lv"] == 2){
-    //SQL作成
-    $dbh = connectDb();
+require_once("lib/function.php");
+$dbh = connectDb();
 
-    //初回アクセス時
-    if(empty($_POST)){
-        try {
-            $sql = "SELECT * FROM ffs_db.user ";
-            $sth = $dbh->prepare($sql);
+if(empty($_POST)){
+    try {
+        $sql = "SELECT * FROM user WHERE acess_lv = 2";
+        $sth = $dbh->prepare($sql);
 
-            $sth->execute();
-        } catch (PDOException $e) {
-            exit("SQL発行エラー：{$e->getMessage()}");
-        }
-    }else{
-        //検索ボタン押下時の処理
-        try {
-            $sql = "SELECT * FROM ffs_db.user WHERE user_name LIKE :search";
-            $sth = $dbh->prepare($sql);
-
-            // プレースホルダに値をバインド
-            $search_name = "%" . $_POST["search"] . "%";
-            $sth->bindValue(":search", $search_user);
-
-            $sth->execute();
-        } catch (PDOException $e) {
-            exit("SQL発行エラー：{$e->getMessage()}");
-        }
+        $sth->execute();
+    } catch (PDOException $e) {
+        exit("SQL発行エラー：{$e->getMessage()}");
     }
-    require_once("lib/view/food/view_user_list.php");
- }
+  }else{
+    //検索ボタン押下時の処理
+    try {
+        $sql = "SELECT * FROM ffs_db.user WHERE name_family LIKE :search";
+        $sth = $dbh->prepare($sql);
+
+        // プレースホルダに値をバインド
+        $search_name = "%" . $_POST["search"] . "%";
+        $sth->bindValue(":search", $search_name);
+
+        $sth->execute();
+    } catch (PDOException $e) {
+        exit("SQL発行エラー：{$e->getMessage()}");
+    }
+
+  }
+    require_once("lib/view/user/view_user_list.php");
 
 ?>
--- user_list_admin.php
+++ user_list_admin.php
@@ -13,13 +13,10 @@
  * バージョン： 1.1
  */
 
-?>
+require_once("lib/function.php");
+$dbh = connectDb();
 
-<?php
-
-    require_once("lib/function.php");
-    $dbh = connectDb();
-
+if(empty($_POST)){
     try {
         $sql = "SELECT * FROM user WHERE acess_lv = 1";
         $sth = $dbh->prepare($sql);
@@ -28,6 +25,22 @@
     } catch (PDOException $e) {
         exit("SQL発行エラー：{$e->getMessage()}");
     }
+  }else{
+    //検索ボタン押下時の処理
+    try {
+        $sql = "SELECT * FROM ffs_db.user WHERE name_family LIKE :search";
+        $sth = $dbh->prepare($sql);
+
+        // プレースホルダに値をバインド
+        $search_name = "%" . $_POST["search"] . "%";
+        $sth->bindValue(":search", $search_name);
+
+        $sth->execute();
+    } catch (PDOException $e) {
+        exit("SQL発行エラー：{$e->getMessage()}");
+    }
+
+  }
     require_once("lib/view/user/view_user_list.php");
 
 ?>
