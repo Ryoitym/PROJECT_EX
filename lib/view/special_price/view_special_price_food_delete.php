78c14afe9b7d17123fac8e12bb48c02e382282ab
nosu10101 <50618690+nosu10101@users.noreply.github.com>
2019-05-27 16:41:51 +0900

    特価商品削除画面--リダイレクト機能追加_特価商品一覧画面(店長)--XSS対策_ph関数へ置き換え

--- lib/view/special_price/view_special_price_food_delete.php
+++ lib/view/special_price/view_special_price_food_delete.php
@@ -7,7 +7,7 @@
  * システム名： FFS
  * 作成者：　nosu10101
  * 作成日：　2019/05/23
- * 最終更新日：　2019/05/24
+ * 最終更新日：　2019/05/27
  * レビュー担当者：
  * レビュー日：
  * バージョン： 1.1
@@ -17,13 +17,14 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>特価商品削除確認画面</title>
+        <title>特価商品削除結果画面</title>
     </head>
 <body>
     <form action="logout.php" method="get">
         <button type="submit" name="logout" value="logout">ログアウト</button>
     </form>
     <p>削除しました。</p>
+    <?php header("refresh:1; url=special_price_food_list.php"); ?>
     <p><a href="special_price_food_list.php">特価商品一覧画面に戻る</a></p>
 </body>
 </html>
