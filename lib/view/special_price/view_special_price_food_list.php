25a766d4f2d1be88b68b88f57c7c570320d772cd
nosu10101 <50618690+nosu10101@users.noreply.github.com>
2019-05-27 13:28:51 +0900

    特価商品一覧画面(店長)---メニュー一覧のリンクを編集

--- lib/view/special_price/view_special_price_food_list.php
+++ lib/view/special_price/view_special_price_food_list.php
@@ -23,10 +23,10 @@
 
     <body>
         <input type="submit" value="ログアウト">
-        <a href="">特価商品</a>
-        <a href="">生鮮食品</a>
-        <a href="">ユーザ</a>
-        <a href="">店舗</a>
+        <a href="special_price_food_list.php">特価商品</a>
+        <a href="food_list.php">生鮮食品</a>
+        <a href="user_list.php">ユーザ</a>
+        <a href="shop_list.php">店舗</a>
 
         <form action="special_price_food_list.php" method="get">
             検索: <input type="text" name="search_name">
